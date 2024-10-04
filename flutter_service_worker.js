f8aff0436b96e0e36eeefe652fcb9e4514623c4b
RizalAnshori <mrizalalanshori@gmail.com>
2024-10-04 15:18:40 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,8 +54,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "39b6d41ecef04ae8ca6f894f95f8a509
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "499f247ea74043df8982c0d8c9e50d73",
-"/": "499f247ea74043df8982c0d8c9e50d73",
+"index.html": "41695ab6d94c85eedc490ac2c72584a1",
+"/": "41695ab6d94c85eedc490ac2c72584a1",
 "main.dart.js": "02b88730c45181985e258777694f81fd",
 "manifest.json": "000f4676d7864452e10e7eb7f09d51fe",
 "version.json": "ac318350482e278d6ed72c64c542867e"};
